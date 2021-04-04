1b5994be9f108d1502b35b728d097b543ac6d8fb
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-04-04 16:14:34 -0400

    Uploaded package on use and pub use keywords

--- learn_7_5_keyword_use/src/lib.rs
+++ learn_7_5_keyword_use/src/lib.rs
@@ -1,27 +1,41 @@
-/** 7_4_path_referring.rs
-  * absolute path starts from a carte root with carte name or a literal carte
-  * relative path starts from current module and used self, super to refer
-  * both absolute and relative path are followed by one or more identifiers separated by::
+/** lib.rs
+  * parent and child module related to the
+  * context of a restaurant
+  * this is brought from package learn_7_4_path_referring/src/main.rs
   *
   * Xuhua Huang
-  * March 2021
+  * April 2021
   */
 
+// module "front_of_house" contains other modules
+// parent module
 mod front_of_house {
-    // pub keyword is necessary or line 21 and 23 will give error
+    // pub keyword is necessary
     pub mod hosting {
         // without pub keyword, it will be a private function
+        pub fn get_empty_space() -> bool {
+            println!("front_of_house::[pub]hosting::[pub]fn get_empty_space");
+            println!("If there is an empty space, return true; if not, return false");
+            true
+        }
+
         pub fn add_to_waitlist() {
             println!("front_of_house::[pub]hosting::[pub]fn add_to_waitlist");
+        }
+
+        pub fn seat_at_table() {
+            println!("front_of_house::[pub]hosting::[pub]fn seat_at_table");
         }
     }
 }
 
+// parent module
 mod back_of_house {
     fn fix_incorrect_order() {
         // calling private function cook_order() within the same module
         cook_order();
         // super brings the path back the file which contains module back_of_house
+        // lib.rs global function
         super::serve_order();
     }
 
@@ -42,12 +56,13 @@ mod back_of_house {
     } // end impl breakfast
 
     pub enum Appetizer {
-    Soup,
-    Salad,
+        Soup,
+        Salad,
     }
 }
 
 // global function
+// without keyword "pub", the following function is private
 fn serve_order() {}
 
 // pub keyword makes an item public
@@ -68,8 +83,23 @@ pub fn eat_at_restaurant() {
     let order2 = back_of_house::Appetizer::Salad;
 }
 
-fn main() {
-    println!("Let's talk about referring path to a module in Rust!");
+pub fn eat_at_restaurant_7_5_use()
+{
+    // this is an implementation restricted to this package
+    // with one of the two lines listed in the following, bring crate front_of_house::hosting into
+    // scope
+    use crate::front_of_house::hosting; // absolute path
+    // use self::front_of_house::hosting; // relative path
+    hosting::add_to_waitlist();
 
-    eat_at_restaurant();
+    // idiomatic use paths
+    use crate::front_of_house::hosting::add_to_waitlist;
+    add_to_waitlist();
+}
+
+// re-exporting name with keyword "pub use"
+pub use crate::front_of_house::hosting;
+// member functions under mod hosting will be available globally
+pub fn eat_at_restaurant_7_5_pub_use() {
+    hosting::add_to_waitlist();
 }
