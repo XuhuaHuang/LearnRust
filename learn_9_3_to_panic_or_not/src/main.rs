83e419b0272167d39ec054f5baa6c652d6b34f5f
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-07-24 21:54:25 -0400

    Rearrange the file. Yes, this repo is still alive...

--- learn_9_3_to_panic_or_not/src/main.rs
+++ learn_9_3_to_panic_or_not/src/main.rs
@@ -1,11 +1,35 @@
+/** 9_3_to_panic_or_not.rs
+ * Conditional panic! macro usage in Rust
+ *
+ * Xuhua Huang
+ * Created: May 2021
+Last updated: July 2021
+ */
+
 use std::cmp::Ordering;
 
-/** 9_3_to_panic_or_not.rs
-  * Conditional panic! marco usage in Rust
-  *
-  * Xuhua Huang
-  * May 2021
-  */
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    // Guess::new()
+    pub fn new(value: i32) -> Guess {
+        // test incoming value
+        // throw a panic! if the test failed
+        // condition may vary
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100. Got {}", value);
+        }
+
+        Guess(value)
+    }
+
+    // function to get value
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
 fn main() {
     println!("Let's talk about panic condition in Rust!");
@@ -51,27 +75,4 @@ fn main() {
     // if there is a state in the program than it can't handle
     // or needs to stop instead of trying to proceed with invalid values
     // UPCOMING: generic types, traits and lifetime specifiers
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    // Guess::new()
-    pub fn new(value: i32) -> Guess {
-        // test incoming value
-        // throw a panic! if the test failed
-        // condition may vary
-        if value < 1 || value > 100 {
-            panic!("Guess must be between 1 and 100. Got {}", value);
-        }
-
-        Guess(value)
-    }
-
-    // function to get value
-    pub fn value(&self) -> i32 {
-        self.value
-    }
 }
