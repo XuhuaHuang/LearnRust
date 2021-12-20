3b89b8ed60e709b0d3abed457278226af7ea40e4
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-12-19 21:22:38 -0500

    Update main.rs

--- learn_1_1_hello_world/src/main.rs
+++ learn_1_1_hello_world/src/main.rs
@@ -1,3 +1,16 @@
+/**
+ * @file main.rs
+ * @author Xuhua Huang (xuhuahuang0412@gmail.com)
+ * @brief The classic "Hello, world" program in Rust
+ * prints greeting in the console
+ * 
+ * @version 0.1
+ * @date 2021-12-19
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 fn main() {
     println!("Hello, world!"); // ! stands fot marco instead of function
 }
