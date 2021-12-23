375230ca587e1c8c318b803819f3b0355804c1fb
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-12-22 20:32:33 -0500

    Create _001_hello_world.rs

---
+++ nerdy_rust_101/_001_hello_world.rs
@@ -0,0 +1,9 @@
+// _001_hello_world.rs
+// to compile and run, execute:
+// rustc _001_hello_world.rs
+// ./_001_hello_world
+
+fn main() {
+    let str = "Hello, world!";
+    println!("{}", str);
+}
