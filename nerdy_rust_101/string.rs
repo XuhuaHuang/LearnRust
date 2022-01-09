9a8360dc979663851528b52fc8b0d5d5c5306260
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-01-09 11:21:17 -0500

    Create string.rs

---
+++ nerdy_rust_101/string.rs
@@ -0,0 +1,14 @@
+// string.rs
+// brief: explore string (both string literals &str and String objects) in Rust
+// to compile and run, execute:
+// rustc string.rs
+// ./string
+
+fn main() {
+    let greeting_question: &str = "How are you doing?"; // &str type; a pointer to a character pointer.
+    // &str objects are allocated on the stack.
+    // `String` objects are allocated on the heap.
+    let person: String = "Andy".to_string();
+    
+    println!("Hello {}\n{}", person, greeting_question);
+}
