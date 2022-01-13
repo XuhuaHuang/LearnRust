effb3c7fdd180c5888f44cada858e734960b43c8
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-01-13 13:00:10 -0500

    Create if_else.rs

---
+++ nerdy_rust_101/if_else.rs
@@ -0,0 +1,12 @@
+/**
+ * if_else.rs
+ * brief: explore conditional statment in Rust; to compile and run, execute:
+ * rustc if_else.rs
+ * ./if_else
+ */
+
+ /**
+  * About conditions in Rust
+  * if-else keywords in Rust aren't neccessarily statements but rather an expression
+  * statements do not return, expressions do; and if-else in Rust always returns SOME value
+  */
