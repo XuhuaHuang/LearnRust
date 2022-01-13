e33f085fcfd506641890fbc0af736daf2910ac30
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-01-13 13:37:09 -0500

    Update if_else.rs

--- nerdy_rust_101/if_else.rs
+++ nerdy_rust_101/if_else.rs
@@ -1,8 +1,8 @@
 /**
  * if_else.rs
  * brief: explore conditional statment in Rust; to compile and run, execute:
- * rustc if_else.rs
- * ./if_else
+ * $ rustc if_else.rs
+ * $ ./if_else
  */
 
  /**
@@ -10,3 +10,21 @@
   * if-else keywords in Rust aren't neccessarily statements but rather an expression
   * statements do not return, expressions do; and if-else in Rust always returns SOME value
   */
+
+fn main() {
+/* Simple demonstration */
+let rust_is_awesome: bool = true;
+if rust_is_awesome {
+    println!("Indeed Rust is awesome!");
+} else {
+    println!("Well, you should give it a try!");
+}
+
+/* If and variable assignment */
+let result: &str = if 1 == 2 {
+    "Wait, hold on, this doesn't make sense."
+} else {
+    "Rust actually makes sense." // this value will be assigned
+};
+println!("Result of the comparison: {:#?}", result);
+}
