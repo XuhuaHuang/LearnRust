1ab47a86ee0b6be95ea5319977c1f5e0ffddd132
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-01-17 08:24:23 -0500

    Create match_expression.rs

---
+++ nerdy_rust_101/match_expression.rs
@@ -0,0 +1,14 @@
+/**
+ * match_expression.rs
+ * brief: filter-like keyword `match` and `=>` operand in Rust; to compile and run, execute:
+ * $ rustc match_expression.rs
+ * $ ./match_expression
+ */
+
+fn request_status() -> u32 {
+    200
+}
+
+fn main() {
+
+}
