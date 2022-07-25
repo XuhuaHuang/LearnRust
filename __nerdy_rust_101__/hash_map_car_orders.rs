fb80882e588c81af43ae37eeedf454be7d8dd129
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-07-24 20:53:32 -0400

    Create hash_map_car_orders.rs

---
+++ __nerdy_rust_101__/hash_map_car_orders.rs
@@ -0,0 +1,9 @@
+// hash_map_car_orders.rs
+// brief: demonstration - track car orders with hash map in Rust
+// to compile and run, execute:
+// rustc hash_map_car_orders.rs -o car_orders.exe
+// ./car_orders
+
+fn main() {
+    
+}
