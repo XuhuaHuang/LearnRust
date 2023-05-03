f4dab342f81e8461f8ee718ceed86d37e30fe84f
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-05-03 11:14:01 -0400

    Update main.rs

--- learn_13_1_closures/src/main.rs
+++ learn_13_1_closures/src/main.rs
@@ -99,10 +99,19 @@ fn main() {
     borrows_mutably();
     println!("After calling borrows_mutably closure: {:?}", list);
 
-    let mut list = [
-        Rectangle { width: 10, height: 1 },
-        Rectangle { width: 3, height: 5 },
-        Rectangle { width: 7, height: 12 },
+    let mut list: [Rectangle; 3] = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
     ];
 
     list.sort_by_key(|r: &Rectangle| r.width);
