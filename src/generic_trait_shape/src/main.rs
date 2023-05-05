d1929caa09be7bf3798dc5c8f518d31872495d4b
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-05-05 15:34:42 -0400

    Update main.rs

--- src/generic_trait_shape/src/main.rs
+++ src/generic_trait_shape/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+
+    #[derive(Debug)]
+    struct GenericRectangle<T, U> {
+        length: T,
+        height: U,
+    }
+
+    let rec: GenericRectangle<i32, f64> = GenericRectangle {
+        length: 4,
+        height: 10.5,
+    };
+    assert!(rec.length == 4);
+    assert!(rec.height == 10.5);
+    println!("{:#?}", rec);
+
+    #[derive(Debug)]
+    struct Rectangle {
+        lengh: f32,
+        width: f32,
+    }
+
+    trait Shape {
+        fn new(lengh: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
+    }
+
+    impl Shape for Rectangle {
+        fn new(lengh: f32, width: f32) -> Rectangle {
+            Rectangle { lengh, width }
+        }
+
+        fn area(&self) -> f32 {
+            self.lengh * self.width
+        }
+    }
+
+    let rec: Rectangle = Shape::new(10.0, 10.0);
+    assert!(rec.area() == 100.0);
+    println!("{:#?}", rec);
 }
