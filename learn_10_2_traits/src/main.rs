c785ec44ea0b0b7262797383c036effe36ccd110
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-05-06 00:15:02 -0400

    Update main.rs

--- learn_10_2_traits/src/main.rs
+++ learn_10_2_traits/src/main.rs
@@ -1,31 +1,24 @@
 /** 10_2_traits.rs
-*
-* Topic: traits in Rust
-* Tells the Rust compiler about functionality a particular type has
-* and can share with other types
-* example: media aggregator library
-*
-* Xuhua Huang
-* Created: Aug 1, 2021
-* Last updated: Aug 2, 2021
+ *
+ * Topic: traits in Rust
+ * Tells the Rust compiler about functionality a particular type has
+ * and can share with other types
+ * example: media aggregator library
+ *
+ * Xuhua Huang
+ * Created: Aug 1, 2021
+ * Last updated: Aug 2, 2021
  */
-
-use learn_10_2_traits::{
-    Tweet,
-    NewsArticle,
-    Summary
-};
+use learn_10_2_traits::{NewsArticle, Summary, Tweet};
 
 fn main() {
     println!(); // added this python style of new line
     println!("Let's talk about Traits - Shared Behaviours in Rust!");
 
     /* Using NewsArticle and Tweet struct with implemented trait */
-    let my_tweet = Tweet {
-        username: String::from("XuhuaHuang"),
-        content: String::from(
-            "Of course, you probably already know"
-        ),
+    let my_tweet: Tweet = Tweet {
+        username: String::from("Xuhua Huang"),
+        content: String::from("Of course, you probably already know"),
         reply: false,
         retweet: false,
     };
@@ -33,8 +26,8 @@ fn main() {
 
     /* Using NewsArticle and NewsArticle struct with implemented trait */
     /* Also with default implementation */
-    let new_article  = NewsArticle {
-        headline: String::from("Chine just earned its 60thOlympic metal in Tokyo 2020!"),
+    let new_article: NewsArticle = NewsArticle {
+        headline: String::from("China just earned its 60th Olympic metal in Tokyo 2020!"),
         location: String::from("Tokyo, Japan"),
         author: String::from("Xuhua Huang"),
         content: String::from(
