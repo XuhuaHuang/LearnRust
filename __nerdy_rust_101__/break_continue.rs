88b0924843f0d0aae7c6cfd51ceeceb9eacafc9d
XuhuaHuang <xuhua.huang.io@gmail.com>
2024-09-30 13:13:05 -0400

    Create break_continue.rs

---
+++ __nerdy_rust_101__/break_continue.rs
@@ -0,0 +1,32 @@
+/**
+ * Break and Continue
+ * rustc break_continue.rs
+ * ./break_continue
+ */
+
+fn main() {
+    let mut i = 0;
+    loop {
+        i += 1;
+        if i > 5 {
+            break;
+        }
+        if i % 2 == 0 {
+            continue;
+        }
+        println!("{}", i);
+    }
+
+    let s = [[5, 6, 7], [8, 9, 10], [21, 15, 32]];
+    let mut elements_searched = 0;
+    let target_value = 10;
+    'outer: for i in 0..=2 {
+        for j in 0..=2 {
+            elements_searched += 1;
+            if s[i][j] == target_value {
+                break 'outer;
+            }
+        }
+    }
+    print!("elements searched: {elements_searched}");
+}
